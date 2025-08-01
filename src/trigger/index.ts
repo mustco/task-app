a0e6aec28f6e85e023da6e6a20b70b4b66184dc6
unknown <mustofaadnan98@gmail.com>
2025-08-01 15:25:46 +0700

    netlify

--- src/trigger/index.ts
+++ src/trigger/index.ts
@@ -3,6 +3,7 @@
 // Export semua tasks
 export * from "./task";
 export * from "./scheduler"; // Export background scheduler job
+export * from "./updateScheduler"; // Export background update scheduler job
 
 // Jika ada tasks lain, export juga di sini
 // export * from "./other-tasks";
